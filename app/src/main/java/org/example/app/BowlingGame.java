eb3ff5f719cf27ec2a60df9b0748599604297869
Olive Nives <olivenives63@gmail.com>
2024-04-12 12:19:51 +0300

    add test for score when there's spare

--- app/src/main/java/org/example/app/BowlingGame.java
+++ app/src/main/java/org/example/app/BowlingGame.java
@@ -4,21 +4,30 @@ package org.example.app;
 
 
 public class BowlingGame {
-    private int rowIndex = 0;
+    private int rollIndex = 0;
     private int[] rolls = new int[21];
     private int score = 0;
 
     // roll method to record rolls in the rolls array
     public void roll(int pinsHit){
-        rolls[rowIndex] = pinsHit;
-        rowIndex++;
+        rolls[rollIndex] = pinsHit;
+        rollIndex++;
     }
 
     // score method to calculate score
     public int score(){
         int score = 0;
-        for (int i = 0; i < rolls.length; i++){
-            score += rolls[i];
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++){
+            
+            if (rolls[frameIndex] + rolls[frameIndex + 1]== 10){// check if spare
+                score += 10 + rolls[frameIndex+2]; // sum of the total in fram (10) plus the score of the next roll
+                frameIndex += 2;
+            }else{
+                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+            
         }
         return score;
     }
