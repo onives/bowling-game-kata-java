dea1bf458c931f47b75f1e182ee42f446d4725e0
Olive Nives <olivenives63@gmail.com>
2024-04-12 09:33:26 +0300

    add refactor before each

--- app/src/test/java/org/example/app/BowlingGameTest.java
+++ app/src/test/java/org/example/app/BowlingGameTest.java
@@ -3,6 +3,7 @@
 */
 package test.java.org.example.app;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.example.app.BowlingGame;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,22 +11,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BowlingGameTest {
+    private BowlingGame game;
+
+    @BeforeEach
+    public void setUp(){
+        game = new BowlingGame();
+    }
+
     @Test
     @DisplayName("Can roll pins")
     public void canRollPins(){
-        BowlingGame game = new BowlingGame();
         game.roll(0);
     }
+
     @Test
     @DisplayName("Can score a gutter game")
     public void canScoreGutterGame(){
-        BowlingGame game = new BowlingGame();
         assertEquals(0, game.score());
     }
+
     @Test
     @DisplayName("Can score a game of ones")
     public void canScoreAgameOfOnes(){
-        BowlingGame game = new BowlingGame();
         for (int i = 0; i<20; i++){
             game.roll(1);
         }
